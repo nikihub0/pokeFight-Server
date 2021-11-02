b540c7379a15b9ca3895caa03dcec6fa1cf91f7c
Nikoletta Geipel <nikolettageipel@MBPvonNikoletta.home>
2021-11-02 17:43:39 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+/*Create a GET route on /pokemon which gives the complete list of pokemon from the JSON
+Create a GET route on /pokemon/:id which gives only one pokemon from the JSON thanks to its id
+(Optional) Create a GET route on /pokemon/:id/:info (<name>|<type>|<base>) which gives only one pokemon from the JSON thanks to its id and retrieve only one information (name or type or base) to send back to the client
+(Optional): Use the MVC pattern; your routes should be managed by a router; which in return triggers a controller that will access the correct Pokemon resources
+Enable all the CORS: https://expressjs.com/en/resources/middleware/cors.html */

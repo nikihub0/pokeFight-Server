00b417e78d8fd554216752406dcd6815a197b0d2
Nikoletta Geipel <nikolettageipel@MBPvonNikoletta.home>
2021-11-03 15:46:08 +0100

    routes

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
+import express from "express";
+import cors from "cors";
+import fs from "fs";
 
+const app = express();
+const port = 8000;
+let rawdata = fs.readFileSync("./pokedex.json");
+let pokemonData = JSON.parse(rawdata);
+//console.log(pokemonData);
 
+app.use(cors());
+
+app.get("/", (req, res) => {
+  console.log("Dies ist die root route");
+});
+
+app.get("/pokemon", (req, res) => {
+  res.json(pokemonData);
+});
+
+app.get("/pokemon/:id", (req, res) => {
+  let { id } = req.params;
+  const pokemon = pokemonData.find((pokemon) => pokemon.id === parseInt(id));
+
+  res.json(pokemon);
+});
+
+app.get("/pokemon/:id/:info", (req, res) => {
+  let { id, info } = req.params;
+  const pokemon = pokemonData.find((pokemon) => pokemon.id === parseInt(id));
+
+  res.json(pokemon[info]);
+});
+
+for (const [key, value] of Object.entries(pokemonData)) {
+  console.log(value.type[0]);
+}
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 /*Create a GET route on /pokemon which gives the complete list of pokemon from the JSON
 Create a GET route on /pokemon/:id which gives only one pokemon from the JSON thanks to its id
 (Optional) Create a GET route on /pokemon/:id/:info (<name>|<type>|<base>) which gives only one pokemon from the JSON thanks to its id and retrieve only one information (name or type or base) to send back to the client
